5f8df94f695bf1f3a9970f4e9c749fe3b1ba1c86
杨凌 <yangling@yanglingdeMacBook-Air.local>
2014-06-12 23:56:25 +0800

    批量导出png

---
+++ exportpng.rb
@@ -0,0 +1,79 @@
+
+
+def extractPng(outFile)
+
+	 keys = {
+	   :filename => outFile,
+	   :width => 640,
+	   :height => 480,
+	   :antialias => false,
+	   :compression => 0.9,
+	   :transparent => true
+	 }
+	 model = Sketchup.active_model
+	 view = model.active_view
+	 view.write_image keys
+end
+
+
+def skp2png(inFile)
+	result = true
+	folder = File.dirname(inFile)
+	fileName = File.basename(inFile, ".skp")
+	outFile = "#{folder}/#{fileName}.png"
+
+
+	result = Sketchup.open_file(inFile)
+	
+	
+
+	#model = Sketchup.active_model
+	#result = model.import inFile, true
+	if result
+		extractPng(outFile)
+	end
+	#Sketchup.quit
+ 	#status = model.close_active
+ 	#model.entities.clear!
+end
+
+
+#define a recursive function that will traverse the directory tree
+def recurFolder(folder)
+	pattern = /.+\.skp$/
+  #we keep track of the directories, to be used in the second, recursive part of this function
+  directories=[]
+  Dir["#{folder}/*"].sort.each do |name|
+  	print name
+    if File.file?(name) and name[pattern]
+    	file = File.expand_path(name)
+    	print file
+    	skp2png(file)
+    elsif File.directory?(name)
+    	directories << name
+    end
+  end
+  directories.each do |name|
+    #don't descend into . or .. on linux
+    Dir.chdir(name){recurFolder(name)} if !Dir.pwd[File.expand_path(name)]
+  end
+end
+
+
+def exportPng()
+	folders = UI.inputbox(["目录："])
+	folder = folders[0]
+	folder = folder.gsub('\\', "/")
+
+
+	recurFolder(folder)
+end
+
+
+
+sn = File.basename(__FILE__)
+unless file_loaded?(sn)
+   UI.menu("Plugins").add_item("export_png") { exportPng }
+   file_loaded(sn)
+end
+
