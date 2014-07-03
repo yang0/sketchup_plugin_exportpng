8cbb0f8fabc151a070e8b0c909e9b742d48b8443
yang0 <yang0@126.com>
2014-07-03 17:53:38 +0800

    duc project

--- exportpng.rb
+++ exportpng.rb
@@ -1,3 +1,4 @@
+
 #作者：旺德福  137962@qq.com
 
 def extractPng(inFile, width, height)
@@ -20,6 +21,7 @@ end
 
 
 def skp2png(inFile)
+	print "skp2png start\n"
 	result = true
 
 
@@ -31,13 +33,17 @@ def skp2png(inFile)
 	#model = Sketchup.active_model
 	#result = model.import inFile, true
 	if result
+		print "file opened\n"
 		extractPng(inFile, 1280, 800)
 		extractPng(inFile, 1000, 1000)
+	else
+		print "file not open\n"
 	end
 	#Sketchup.quit
  	#status = model.close_active
  	#model.entities.clear!
 end
+
 
 
 #define a recursive function that will traverse the directory tree
@@ -46,13 +52,25 @@ def recurFolder(folder)
   #we keep track of the directories, to be used in the second, recursive part of this function
   directories=[]
   Dir["#{folder}/*"].sort.each do |name|
+  	print "\n==================================\n"
   	print name
-    if File.file?(name) and name[pattern]
-    	file = File.expand_path(name)
-    	print file
-    	skp2png(file)
+
+  	name = name.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+
+    if name[pattern]
+    	if File.file?(name)
+	    	print "expand_path start\n"
+	    	file = File.expand_path(name)
+	    	print file
+	    	skp2png(file)
+	    else
+	    	print "\n模式匹配但是非文件\n"
+	    end
     elsif File.directory?(name)
+    	print "dir\n"
     	directories << name
+    else
+    	print "\n not skp file or dir\n"
     end
   end
   directories.each do |name|
